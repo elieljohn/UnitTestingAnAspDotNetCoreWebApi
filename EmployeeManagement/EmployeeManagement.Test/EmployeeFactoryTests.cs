ab3d7576feaefb37e849774149e0bd370d75e001
elieljohn <ejmalabar88@gmail.com>
2024-10-02 17:36:03 +0100

    Writing Your First Unit Test
    1. Create EmployeeManagement.Test project.
    
    2. Build EmployeeFactoryTests.cs.
    
    3. Build CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500 test, and decorate with Fact attribute to indicate that it is a test.
    EmployeeFactoryTests.cs: line 14-23

---
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory
+                .CreateEmployee("Kevin", "Dockx");
+
+            Assert.Equal(3000, employee.Salary);
+        }
+    }
+}
