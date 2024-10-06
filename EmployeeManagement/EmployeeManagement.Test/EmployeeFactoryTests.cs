2910e7647b84ca5e0c8a2294d4ae2bfeac315686
elieljohn <ejmalabar88@gmail.com>
2024-10-06 13:31:24 +0100

    Asserting on Floating Points with Precision
    1. Add CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample test.
    EmployeeFactoryTests.cs: line 66-78

--- EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -62,5 +62,19 @@ namespace EmployeeManagement.Test
             // Assert
             Assert.InRange(employee.Salary, 2500, 3500);
         }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            employee.Salary = 2500.123m;
+
+            // Assert
+            Assert.Equal(2500, employee.Salary, 0);
+        }
     }
 }
