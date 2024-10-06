7e97048c94d6ee1c5206444791373007b501e11e
elieljohn <ejmalabar88@gmail.com>
2024-10-06 13:28:03 +0100

    Asserting on Numberic Values
    1. Add CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500 test.
    EmployeeFactoryTests.cs: line 25-37
    
    2. Add CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative test.
    EmployeeFactoryTests.cs: line 39-51
    
    3. Add CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange test using InRange method.
    EmployeeFactoryTests.cs: line 53-64

--- EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -21,5 +21,46 @@ namespace EmployeeManagement.Test
 
             Assert.Equal(2500, employee.Salary);
         }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500,
+                "Salary not in acceptable range.");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.True(employee.Salary >= 2500);
+            Assert.True(employee.Salary <= 3500);
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
     }
 }
