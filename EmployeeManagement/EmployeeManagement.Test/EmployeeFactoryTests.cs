10ea4bebc005507e27a39a7600aa370f03f32011
elieljohn <ejmalabar88@gmail.com>
2024-10-09 17:43:48 +0100

    Sharing Context with the Constructor and Dispose Approach
    1. Add an EmployeeFactory private property and instantiate an EmployeeFactory on the EmployeeFactoryTests() constructor.
    EmployeeFactoryTests.cs: line 14-19
    
    2. Implement the IDisposable and its Dispose method.
    EmployeeFactoryTests.cs: line 12, 21-24
    
    3. Remove the arrange step on the tests as the EmployeeFactory is already instantiated on the constructor. Modify the act step to use the _employeeFactory instance.

--- EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -9,14 +9,25 @@ using Xunit;
 
 namespace EmployeeManagement.Test
 {
-    public class EmployeeFactoryTests
+    public class EmployeeFactoryTests : IDisposable
     {
+        private EmployeeFactory _employeeFactory;
+
+        public EmployeeFactoryTests()
+        {
+            _employeeFactory = new EmployeeFactory();
+        }
+
+        public void Dispose()
+        {
+            // clean up the setup code, if required
+        }
+
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
-        {
-            var employeeFactory = new EmployeeFactory();
-
-            var employee = (InternalEmployee)employeeFactory
+        {    
+            
+            var employee = (InternalEmployee)_employeeFactory
                 .CreateEmployee("Kevin", "Dockx");
 
             Assert.Equal(2500, employee.Salary);
@@ -26,10 +37,10 @@ namespace EmployeeManagement.Test
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
         {
             // Arrange
-            var employeeFactory = new EmployeeFactory();
 
             // Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory
+                .CreateEmployee("Kevin", "Dockx");
 
             // Assert
             Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500,
@@ -40,10 +51,10 @@ namespace EmployeeManagement.Test
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
         {
             // Arrange
-            var employeeFactory = new EmployeeFactory();
 
             // Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory
+                .CreateEmployee("Kevin", "Dockx");
 
             // Assert
             Assert.True(employee.Salary >= 2500);
@@ -54,10 +65,10 @@ namespace EmployeeManagement.Test
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
         {
             // Arrange
-            var employeeFactory = new EmployeeFactory();
 
             // Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory
+                .CreateEmployee("Kevin", "Dockx");
 
             // Assert
             Assert.InRange(employee.Salary, 2500, 3500);
@@ -67,10 +78,10 @@ namespace EmployeeManagement.Test
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
         {
             // Arrange
-            var employeeFactory = new EmployeeFactory();
 
             // Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory
+                .CreateEmployee("Kevin", "Dockx");
             employee.Salary = 2500.123m;
 
             // Assert
@@ -81,10 +92,10 @@ namespace EmployeeManagement.Test
         public void CreateEmployee_IsExternalIsTrue_ReturnTypeMustBeExternalEmployee()
         {
             // Arrange
-            var factory = new EmployeeFactory();
 
             // Act
-            var employee = factory.CreateEmployee("Kevin", "Dockx", "Marvin", true);
+            var employee = _employeeFactory
+                .CreateEmployee("Kevin", "Dockx", "Marvin", true);
 
             // Assert
             Assert.IsType<ExternalEmployee>(employee);
