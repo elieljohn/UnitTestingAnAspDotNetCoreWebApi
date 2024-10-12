68b6557d59d1638cc2377ea0063eadbad24d502a
elieljohn <ejmalabar88@gmail.com>
2024-10-12 16:49:24 +0100

    Testing a Theory with Member Data
    1. Add GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue test to DataDrivenEmployeeServiceTests class. This should accept an int value - raiseGiven, and a bool expectedValueForMinimumRaiseGiven. raiseGiven will be used on the act stage, while expectedValueForMinimumRaiseGiven will be used on the assert phase.
    
    2. Add member datas ExampleTestDataForGiveRaise_WithProperty and ExampleTestDataForGiveRaise_WithMethod.
    
    3. Decorate GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue with the Theory attribute, then with MemberData, passing in the member data added earlier.

--- EmployeeManagement/EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement/EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -63,5 +63,50 @@ namespace EmployeeManagement.Test
             // Assert
             Assert.False(internalEmployee.MinimumRaiseGiven);
         }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithProperty
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                        new object[] { 100, true },
+                        new object[] { 200, false }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithMethod(
+            int testDataInstancesToProvide)
+        {
+            var testData = new List<object[]>
+                {
+                        new object[] { 100, true },
+                        new object[] { 200, false }
+                };
+
+            return testData.Take(testDataInstancesToProvide);
+        }
+
+        [Theory]
+        [MemberData(
+            nameof(DataDrivenEmployeeServiceTests.ExampleTestDataForGiveRaise_WithMethod),
+            1,
+            MemberType = typeof(DataDrivenEmployeeServiceTests))]
+        public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(
+            int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+        {
+            // Arrange  
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1);
+
+            // Act
+            await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(
+                internalEmployee, raiseGiven);
+
+            // Assert
+            Assert.Equal(expectedValueForMinimumRaiseGiven,
+                internalEmployee.MinimumRaiseGiven);
+        }
     }
 }
